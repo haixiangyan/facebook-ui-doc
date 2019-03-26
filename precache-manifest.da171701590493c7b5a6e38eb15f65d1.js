e1221465319153b0f5c4482a5e5461d192427a15
Haixiang <haixiang6123@gmail.com>
2019-03-26 11:21:54 -0700

    Updates

---
+++ precache-manifest.da171701590493c7b5a6e38eb15f65d1.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "adf0d9ba2f7ed22307c7",
+    "url": "/facebook-ui-doc/static/js/runtime~main.b7abb701.js"
+  },
+  {
+    "revision": "61f855192a260b481de5",
+    "url": "/facebook-ui-doc/static/js/main.ed0326b2.chunk.js"
+  },
+  {
+    "revision": "c5be6c0c9886ac84dcac",
+    "url": "/facebook-ui-doc/static/js/2.2ae46438.chunk.js"
+  },
+  {
+    "revision": "23d9f52d6afb4e853609a089d0349132",
+    "url": "/facebook-ui-doc/index.html"
+  }
+];
